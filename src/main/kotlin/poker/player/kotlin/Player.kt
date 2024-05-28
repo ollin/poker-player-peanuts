ebc559b532091b7275d08e8e114efe3f084f862f
Oliver Nautsch <oliver@brixel.io>
2024-05-28 11:29:06 +0200

    classes

--- src/main/kotlin/poker/player/kotlin/Player.kt
+++ src/main/kotlin/poker/player/kotlin/Player.kt
@@ -4,6 +4,7 @@ import org.json.JSONObject
 
 class Game {
     fun betRequest(game_state: JSONObject): Int {
+        fromJsonToTournament(game_state)
         return 3000
     }
 
@@ -11,9 +12,59 @@ class Game {
     }
 
     fun version(): String {
-        return "0.0.3 - all in 3000"
+        return "0.0.4 - parse"
+    }
+
+    fun fromJsonToTournament(jsonObject: JSONObject): Tournament {
+
+        val playersJson = jsonObject.getJSONArray("players")
+        val players = mutableListOf<Player>()
+        for (i in 0 until playersJson.length()) {
+            val playerJson = playersJson.getJSONObject(i)
+
+            val cardsJson = playerJson.optJSONArray("hole_cards")
+            val cards = mutableListOf<Card>()
+            if (cardsJson != null) {
+                for (j in 0 until cardsJson.length()) {
+                    val cardJson = cardsJson.getJSONObject(j)
+                    cards.add(Card(cardJson.getString("rank"), cardJson.getString("suit")))
+                }
+            }
+
+            players.add(Player(playerJson.getInt("id"),
+                playerJson.getString("name"),
+                playerJson.getString("status"),
+                playerJson.getString("version"),
+                playerJson.getInt("stack"),
+                playerJson.getInt("bet"),
+                cards)
+            )
+        }
+
+        val communityCardsJson = jsonObject.getJSONArray("community_cards")
+        val communityCards = mutableListOf<Card>()
+        for (i in 0 until communityCardsJson.length()) {
+            val cardJson = communityCardsJson.getJSONObject(i)
+            communityCards.add(Card(cardJson.getString("rank"), cardJson.getString("suit")))
+        }
+
+        return Tournament(jsonObject.getString("tournament_id"),
+            jsonObject.getString("game_id"),
+            jsonObject.getInt("round"),
+            jsonObject.getInt("bet_index"),
+            jsonObject.getInt("small_blind"),
+            jsonObject.getInt("current_buy_in"),
+            jsonObject.getInt("pot"),
+            jsonObject.getInt("minimum_raise"),
+            jsonObject.getInt("dealer"),
+            jsonObject.getInt("orbits"),
+            jsonObject.getInt("in_action"),
+            players,
+            communityCards)
     }
 }
+
+
 
 data class Tournament(
     val tournament_id: String,
