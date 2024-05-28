b9fc500c3bca9ab7259d412363f20622b6390713
Oliver Nautsch <oliver@brixel.io>
2024-05-28 14:26:13 +0200

    hich

--- src/test/kotlin/poker/player/kotlin/GameTest.kt
+++ src/test/kotlin/poker/player/kotlin/GameTest.kt
@@ -7,17 +7,29 @@ import io.kotest.matchers.shouldBe
 class GameTest : StringSpec({
 
     "4 is not high card"{
-
-        val card1 = Card.create("4♥")
-        val card2 = Card.create("4♦")
-
-        val we = Player(
-            hole_cards = listOf(card1, card2)
-        )
-        val tournament = Tournament(
-            players = listOf(we),
-        )
+        val tournament = createStartingHand("4♥", "4♦")
 
         Game().highCard(tournament) shouldBe false
     }
+
+    "J is high card"{
+        val tournament = createStartingHand("J♥", "4♦")
+
+        Game().highCard(tournament) shouldBe true
+    }
+
+
 })
+
+private fun createStartingHand(cardA: String, cardB: String): Tournament {
+    val card1 = Card.create(cardA)
+    val card2 = Card.create(cardB)
+
+    val we = Player(
+        hole_cards = listOf(card1, card2)
+    )
+    val tournament = Tournament(
+        players = listOf(we),
+    )
+    return tournament
+}
